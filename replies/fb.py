aecd95065e398aadb1ae3ccc565682f52df3202d
zxc880809 <103117747+zxc880809@users.noreply.github.com>
2022-05-04 19:36:03 +0800

    Update fb.py

--- replies/fb.py
+++ replies/fb.py
@@ -6,7 +6,7 @@ dict = {
 def reply(input):
     if input == "我要加社團！":
         return[TextSendMessage(text="這裡是成大人必加的臉書社團，快加入吧！😍😍"),
-               TextSendMessage(text="▪成大二手交流版 \n https://www.facebook.com/groups/384397068383429 \n ▪成大選課 \n https://www.facebook.com/groups/637099219647956/ \n ▪NCKU HUB(選課心得分享）\n https://nckuhub.com\n ▪成大租屋 \n https://www.facebook.com/groups/NCKUhouse/ \n 成大講座https://www.facebook.com/groups/762866160499626/" )]
+               TextSendMessage(text="▪成大二手交流版 \n https://www.facebook.com/groups/384397068383429 \n ▪成大選課 \n https://www.facebook.com/groups/637099219647956/ \n ▪NCKU HUB(選課心得分享）\n https://nckuhub.com\n ▪成大租屋 \n https://www.facebook.com/groups/NCKUhouse/ \n ▪成大講座 \n https://www.facebook.com/groups/762866160499626/" )]
     else:
         return "NOT_THIS"
 
