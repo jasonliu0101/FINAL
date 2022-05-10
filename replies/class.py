61f0cbf36a1ce5ca205eaf21cdfd249153016776
alan0421 <72789105+alan0421@users.noreply.github.com>
2022-05-10 11:05:09 +0800

    Update class.py

--- replies/class.py
+++ replies/class.py
@@ -1,45 +1,36 @@
 from linebot.models import *
-dict = {
-    "我要加社團！"
-}
-@handler.add(MessageEvent, message=TextMessage) 
-def handle_message(event):
-    message =event.message.text               #取得使用者訊息
-    if re.match('選課查詢',message):
-        image_message = ImageSendMessage(
-            original_content_url='https://i.imgur.com/Y3DKI9C.jpeg',
-            preview_image_url='https://i.imgur.com/Y3DKI9C.jpeg'
-        )
-        line_bot_api.reply_message(event.reply_token, image_message)
-    elif re.match('單科加選',message):
-        image_message = ImageSendMessage(
-            original_content_url='https://i.imgur.com/rsB1MEZ.jpeg',
-            preview_image_url='https://i.imgur.com/rsB1MEZ.jpeg'
-        )
-        line_bot_api.reply_message(event.reply_token, image_message)   
-    elif re.match('棄選流程',message):
-        image_message = ImageSendMessage(
-            original_content_url='https://i.imgur.com/P9PES7z.jpeg',
-            preview_image_url='https://i.imgur.com/P9PES7z.jpeg'
-        )
-        line_bot_api.reply_message(event.reply_token, image_message) 
-    elif re.match('通識登記一',message):
-        image_message = ImageSendMessage(
-            original_content_url='https://i.imgur.com/gUttt07.jpeg',
-            preview_image_url='https://i.imgur.com/gUttt07.jpeg'
-        )
-        line_bot_api.reply_message(event.reply_token, image_message) 
-    elif re.match('通識登記二',message):
-        image_message = ImageSendMessage(
-            original_content_url='https://i.imgur.com/AEnaRAZ.jpeg',
-            preview_image_url='https://i.imgur.com/AEnaRAZ.jpeg'
-        )
-        line_bot_api.reply_message(event.reply_token, image_message)     
 
+class_dict = {
+    "選課查詢",
+    "單科加選",
+    "棄選流程",
+    "通識登記一",
+    "通識登記二"
+}
+
+
+def reply(input):
+    if input == "選課查詢":
+         return[ImageSendMessage(original_content_url='https://i.imgur.com/Y3DKI9C.jpeg',
+                                 preview_image_url='https://i.imgur.com/Y3DKI9C.jpeg')]
+     
+    elif input == "單科加選":
+         return[ImageSendMessage(original_content_url=''https://i.imgur.com/rsB1MEZ.jpeg',
+                                 preview_image_url=''https://i.imgur.com/rsB1MEZ.jpeg')]
+      
+    elif input == "棄選流程":
+         return[ImageSendMessage(original_content_url=''https://i.imgur.com/P9PES7z.jpeg',
+                                 preview_image_url=''https://i.imgur.com/P9PES7z.jpeg')]
+     
+    elif input == "通識登記一":
+         return[ImageSendMessage(original_content_url=''https://i.imgur.com/gUttt07.jpeg',
+                                 preview_image_url=''https://i.imgur.com/gUttt07.jpeg')]
+      
+    elif input == "通識登記二":
+         return[ImageSendMessage(original_content_url=''https://i.imgur.com/AEnaRAZ.jpeg',
+                                 preview_image_url=''https://i.imgur.com/AEnaRAZ.jpeg')]
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
-
-
 
 
 
