b90f3e322adda31ff9ff3a0ab1b1caf67756a8a9
alan0421 <72789105+alan0421@users.noreply.github.com>
2022-05-10 10:46:54 +0800

    Update class.py

--- replies/class.py
+++ replies/class.py
@@ -1,4 +1,7 @@
-#功能區
+from linebot.models import *
+dict = {
+    "我要加社團！"
+}
 @handler.add(MessageEvent, message=TextMessage) 
 def handle_message(event):
     message =event.message.text               #取得使用者訊息
