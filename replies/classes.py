46495d7ce1a9eaba95f19b0961c3cb5135aedbac
phi622968143 <40814498+phi622968143@users.noreply.github.com>
2022-05-16 22:34:27 +0800

    Update classes.py
    使用flex 新增一格按鈕

--- replies/classes.py
+++ replies/classes.py
@@ -12,55 +12,102 @@ class_dict = {
 
 def reply(input):
     if input == "我要選課！":
-        return [TextSendMessage(text="選課小幫手，請選擇要需要教學的選課流程"),
-                TemplateSendMessage(  # 這個是按鈕樣版訊息的格式
-                    alt_text="template",
-                    template=ButtonsTemplate(
-                        title="請選擇",
-                        text="👇",
-                        actions=[
-                            MessageAction(  # 這邊是在設定按鈕按下以後回傳的是訊息
-                                label="選課查詢",
-                                text="選課查詢",
-                            ),
-                            MessageAction(
-                                label="單科加選",
-                                text="單科加選"
-                            ),
-                            MessageAction(
-                                label="棄選流程",
-                                text="棄選流程",
-                            ),
-                            MessageAction(
-                                label="通識登記一",
-                                text="通識登記一"
-                            ),
-                            # MessageAction(
-                            #     label="通識登記二",
-                            #     text="通識登記二"
-                            # )
-                        ]
+        return testFMS()
 
-                    )
-                )
-                ]
-
-    elif input == "選課查詢":
-        return [ImageSendMessage(original_content_url='https://i.imgur.com/Y3DKI9C.jpeg',
-                                 preview_image_url='https://i.imgur.com/Y3DKI9C.jpeg')]
-
-    elif input == "單科加選":
-        return [ImageSendMessage(original_content_url='https://i.imgur.com/rsB1MEZ.jpeg',
-                                 preview_image_url='https://i.imgur.com/rsB1MEZ.jpeg')]
-
-    elif input == "棄選流程":
-        return [ImageSendMessage(original_content_url='https://i.imgur.com/P9PES7z.jpeg',
-                                 preview_image_url='https://i.imgur.com/P9PES7z.jpeg')]
-
-    elif input == "通識登記一":
-        return [ImageSendMessage(original_content_url='https://i.imgur.com/gUttt07.jpeg',
-                                 preview_image_url='https://i.imgur.com/gUttt07.jpeg')]
-
-    elif input == "通識登記二":
-        return [ImageSendMessage(original_content_url='https://i.imgur.com/AEnaRAZ.jpeg',
-                                 preview_image_url='https://i.imgur.com/AEnaRAZ.jpeg')]
+def testFMS():
+    return [FlexSendMessage(
+        alt_text='Flex Message',
+        contents={
+  "type": "bubble",
+  "hero": {
+    "type": "image",
+    "url": "https://imgur.com/e8zohEF.jpg",
+    "size": "full",
+    "aspectRatio": "20:13",
+    "aspectMode": "cover",
+    "action": {
+      "type": "uri",
+      "uri": "http://linecorp.com/"
+    }
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "NCKU 選課導覽",
+        "weight": "bold",
+        "size": "xxl",
+        "color": "#66DD00"
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "sm",
+    "contents": [
+      {
+        "type": "button",
+        "style": "link",
+        "height": "sm",
+        "action": {
+          "type": "uri",
+          "label": "選課查詢",
+          "uri": "https://i.imgur.com/Y3DKI9C.jpeg"
+        }
+      },
+      {
+        "type": "button",
+        "style": "link",
+        "height": "sm",
+        "action": {
+          "type": "uri",
+          "label": "單科加選",
+          "uri": "https://i.imgur.com/rsB1MEZ.jpeg"
+        },
+        "margin": "md",
+        "offsetBottom": "none",
+        "offsetStart": "none",
+        "offsetEnd": "none",
+        "offsetTop": "none"
+      },
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "棄選流程",
+          "uri": "https://i.imgur.com/P9PES7z.jpeg'"
+        }
+      },
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "通識登記一",
+          "uri": "https://i.imgur.com/gUttt07.jpeg"
+        }
+      },
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "通識登記二",
+          "uri": "https://i.imgur.com/AEnaRAZ.jpeg"
+        }
+      },
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "action",
+          "uri": "http://linecorp.com/"
+        }
+      }
+    ],
+    "flex": 0
+  }
+}
+        
+    )]
